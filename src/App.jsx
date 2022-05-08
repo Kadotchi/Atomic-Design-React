77ccee45d5861c0687d384d585cda9fc862ac5a6
Kadotchi <84917429+Kadotchi@users.noreply.github.com>
2022-05-08 14:32:07 +0900

    Organismの作成1

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import PraimaryButton from "./components/atoms/button/PraimaryButton";
 import SecondaryButton from "./components/atoms/button/SecondaryButton";
 import { SearchInput } from "./components/molecules/SearchInput";
+import { UserCard } from "./components/ouganisms/user/UserCard";
 import "./styles.css";
 
 export default function App() {
@@ -10,6 +11,7 @@ export default function App() {
       <SecondaryButton>検索</SecondaryButton>
       <br />
       <SearchInput />
+      <UserCard />
     </div>
   );
 }
